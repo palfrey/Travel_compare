280c0614919c99401f67a3853a84681cef7873d3
Tom Parker <palfrey@tevp.net>
2009-09-12 11:37:44 +0100

    App now runs under Google App Engine

--- tc.py
+++ tc.py
@@ -9,22 +9,18 @@ import math
 from datetime import date, timedelta
 import pprint
 
-#from sys import path
-#path.append("amee")
-#from amee import AMEEConnection
+import wsgiref.handlers
+from google.appengine.ext import webapp
 
+cache = None
 geocode_url = "http://local.yahooapis.com/MapsService/V1/geocode?appid=%s&location=%s"
 gmaps_url = "http://maps.google.com/maps?f=d&source=s_d&saddr=%s&daddr=%s&hl=en&geocode=&mra=ls&vps=1&output=js"
 
-cache = GetURL()
-
-cp = ConfigParser()
-cp.read("tc.ini")
-
-yahoo_id = cp.get("secrets","yahoo_id")
-assert len(argv) == 3, len(argv)
-
 def loc_info(loc):
+	cp = ConfigParser()
+	cp.read("tc.ini")
+	yahoo_id = cp.get("secrets","yahoo_id")
+
 	start_loc = cache.get(geocode_url%(yahoo_id,quote_plus(loc)), max_age=-1).read()
 
 	dom = parseString(start_loc)
@@ -72,16 +68,6 @@ def directions(start_loc, end_loc):
 	# distance always in km
 	return {"distance": shortest}
 
-start = loc_info(argv[1])
-end = loc_info(argv[2])
-
-print start
-print end
-
-path = directions(start, end)
-
-print path
-
 def trainPerKm(trainfile):
 	dom = parse(trainfile)
 	for p in dom.getElementsByTagName("Path"):
@@ -96,30 +82,6 @@ def planeTotal(planefile):
 			return float(p.parentNode.getElementsByTagName("Value")[0].firstChild.data)
 	raise Exception
 
-national = trainPerKm("train-National")
-international = trainPerKm("train-International")
-
-if start["Country"] == end["Country"]:
-	trainrate = national
-else:
-	trainrate = (national + international) / 2.0
-
-traincost = trainrate*path["distance"]
-
-flights = {
-		"domestic":{"fname":"plane-Domestic","limit":463},
-		"short-haul":{"fname":"plane-ShortHaul","limit":1108},
-		"long-haul":{"fname":"plane-LongHaul","limit":6482}
-		}
-
-kind = None
-for k in sorted(flights.keys(),cmp=lambda x,y:cmp(flights[x]["limit"],flights[y]["limit"])):
-	if path["distance"]<(flights[k]["limit"]*1.5): # 50% extra limit
-		kind = k
-		break
-
-assert kind!=None,path["distance"]
-
 # Distance code from http://www.zachary.com/s/blog/2005/01/12/python_zipcode_geo-programming
 
 #
@@ -127,60 +89,128 @@ assert kind!=None,path["distance"]
 # http://williams.best.vwh.net/avform.htm
 #
 
-nauticalMilePerLat = 60.00721
-nauticalMilePerLongitude = 60.10793
-rad = math.pi / 180.0
-milesPerNauticalMile = 1.15078
+def calcDistance(lat1, lon1, lat2, lon2):					  
+	nauticalMilePerLat = 60.00721
+	nauticalMilePerLongitude = 60.10793
+	rad = math.pi / 180.0
+	milesPerNauticalMile = 1.15078
 
-def calcDistance(lat1, lon1, lat2, lon2):                      
-    """
-    Caclulate distance between two lat lons in NM
-    """
-    yDistance = (lat2 - lat1) * nauticalMilePerLat
-    xDistance = (math.cos(lat1 * rad) + math.cos(lat2 * rad)) * (lon2 - lon1) * (nauticalMilePerLongitude / 2)
+	"""
+	Calculate distance between two lat lons in NM
+	"""
+	yDistance = (lat2 - lat1) * nauticalMilePerLat
+	xDistance = (math.cos(lat1 * rad) + math.cos(lat2 * rad)) * (lon2 - lon1) * (nauticalMilePerLongitude / 2)
 
-    distance = math.sqrt( yDistance**2 + xDistance**2 )
+	distance = math.sqrt( yDistance**2 + xDistance**2 )
 
-    return distance * milesPerNauticalMile
+	return distance * milesPerNauticalMile
 
 # end copy+paste distance code
-
-directDistance = calcDistance(start["Lat"],start["Long"],end["Lat"],end["Long"])*1.09 # add 9% for going in the air! See Plane_generic amee wiki page
 
 def co2InItems(amount, item):
 	data = cache.get("http://carbon.to/%s?co2=%f"%(item,amount)).read()
 	dom = parseString(data)
 	return float(dom.getElementsByTagName("amount")[0].firstChild.data)
 
-flightcost = (planeTotal(flights[k]["fname"])/flights[k]["limit"])*directDistance
-print flightcost,"kg",directDistance,"km"
+class MainHandler(webapp.RequestHandler):
 
-ebookers_url = "http://www.ebookers.com/shop/airsearch?type=air&ar.type=oneWay&ar.ow.leaveSlice.orig.key=%s&ar.ow.leaveSlice.dest.key=%s&ar.ow.leaveSlice.date=%d%%2F%d%%2F%2d&ar.ow.leaveSlice.time=Anytime&ar.ow.numAdult=1&ar.ow.numSenior=0&ar.ow.numChild=0&ar.ow.child%%5B0%%5D=&ar.ow.child%%5B1%%5D=&ar.ow.child%%5B2%%5D=&ar.ow.child%%5B3%%5D=&ar.ow.child%%5B4%%5D=&ar.ow.child%%5B5%%5D=&ar.ow.child%%5B6%%5D=&ar.ow.child%%5B7%%5D=&_ar.ow.nonStop=0&_ar.ow.narrowSel=0&ar.ow.narrow=airlines&ar.ow.carriers%%5B0%%5D=&ar.ow.carriers%%5B1%%5D=&ar.ow.carriers%%5B2%%5D=&ar.ow.cabin=C&search=Search+Flights"
+	def get(self):
+		start = self.request.get("start")
+		end = self.request.get("end")
+		self.response.out.write("""<html>
+			<body>
+			  <form action="/" method="get">
+				<div>Start: <input type="text" name="start" value="%s"</div>
+				<div>End: <input type="text" name="end" value="%s"</div>
+				<div><input type="submit" value="Discover cost"></div>
+			  </form>
+		"""%(start,end))
+		if start == "" or end == "":
+			self.response.out.write("</body></html>")
+			return
+		global cache
+		cache = GetURL()
 
-when = date.today() + timedelta(30) # 30 days time
-ebookers = cache.get(ebookers_url%(start["City"], end["City"], when.day, when.month, when.year)).read()
-open("dump","w").write(ebookers)
-prices = findall("class=\"price\">£([\d,]+\.\d+)\*</span>", ebookers)
-#schedules = findall("<table class=\"airItinerarySummary summary block hideFromNonJS\">(.+?)</table>", ebookers, DOTALL)
-schedules = findall("<td class=\"col5\">(.+?)</td>", ebookers, DOTALL)
-assert len(schedules) == len(prices),(len(schedules),len(prices))
+		start = loc_info(start)
+		end = loc_info(end)
 
-planeprice = float(prices[0])
-planetime = schedules[0].strip()
-if planetime.find(" ")!=-1: # assume hr bit on front
-	hrs = planetime.split(" ")[0].strip()
-	assert hrs[-2:] == "hr",hrs
-	planemins = int(hrs[:-2])*60
-	planetime = planetime.split(" ")[1]
-else:
-	planemins = 0
-assert planetime[-3:] == "min",planetime
-planemins += int(planetime[:-3])
+		#print start
+		#print end
 
-print "planemins",planemins
+		path = directions(start, end)
 
-print
-print "Going from %s -> %s"%(start["Address"], end["Address"])
-print "Train: distance %.1f km, Co2 %.1f kg"%(path["distance"], traincost),"or %d bottles of beer"%co2InItems(traincost, "beers")
-print "Plane: distance %.1f km, Price £%.2f, Time %d minutes, Co2 %.1f kg"%(directDistance, planeprice, planemins, flightcost),"or %d bottles of beer"%co2InItems(flightcost, "beers")
-print "Plane is %.1f%% worse than train"%(((flightcost-traincost)/traincost)*100.0)
+		#print path
+
+		national = trainPerKm("train-National")
+		international = trainPerKm("train-International")
+
+		if start["Country"] == end["Country"]:
+			trainrate = national
+		else:
+			trainrate = (national + international) / 2.0
+
+		traincost = trainrate*path["distance"]
+
+		flights = {
+				"domestic":{"fname":"plane-Domestic","limit":463},
+				"short-haul":{"fname":"plane-ShortHaul","limit":1108},
+				"long-haul":{"fname":"plane-LongHaul","limit":6482}
+				}
+
+		kind = None
+		for k in sorted(flights.keys(),cmp=lambda x,y:cmp(flights[x]["limit"],flights[y]["limit"])):
+			if path["distance"]<(flights[k]["limit"]*1.5): # 50% extra limit
+				kind = k
+				break
+
+		assert kind!=None,path["distance"]
+
+		directDistance = calcDistance(start["Lat"],start["Long"],end["Lat"],end["Long"])*1.09 # add 9% for going in the air! See Plane_generic amee wiki page
+
+		flightcost = (planeTotal(flights[k]["fname"])/flights[k]["limit"])*directDistance
+		#print flightcost,"kg",directDistance,"km"
+
+		ebookers_url = "http://www.ebookers.com/shop/airsearch?type=air&ar.type=oneWay&ar.ow.leaveSlice.orig.key=%s&ar.ow.leaveSlice.dest.key=%s&ar.ow.leaveSlice.date=%d%%2F%d%%2F%2d&ar.ow.leaveSlice.time=Anytime&ar.ow.numAdult=1&ar.ow.numSenior=0&ar.ow.numChild=0&ar.ow.child%%5B0%%5D=&ar.ow.child%%5B1%%5D=&ar.ow.child%%5B2%%5D=&ar.ow.child%%5B3%%5D=&ar.ow.child%%5B4%%5D=&ar.ow.child%%5B5%%5D=&ar.ow.child%%5B6%%5D=&ar.ow.child%%5B7%%5D=&_ar.ow.nonStop=0&_ar.ow.narrowSel=0&ar.ow.narrow=airlines&ar.ow.carriers%%5B0%%5D=&ar.ow.carriers%%5B1%%5D=&ar.ow.carriers%%5B2%%5D=&ar.ow.cabin=C&search=Search+Flights"
+
+		when = date.today() + timedelta(30) # 30 days time
+		ebookers = cache.get(ebookers_url%(start["City"], end["City"], when.day, when.month, when.year)).read()
+		#open("dump","w").write(ebookers)
+		prices = findall("class=\"price\">£([\d,]+\.\d+)\*</span>", ebookers)
+		#schedules = findall("<table class=\"airItinerarySummary summary block hideFromNonJS\">(.+?)</table>", ebookers, DOTALL)
+		schedules = findall("<td class=\"col5\">(.+?)</td>", ebookers, DOTALL)
+		assert len(schedules) == len(prices),(len(schedules),len(prices))
+
+		planeprice = float(prices[0])
+		planetime = schedules[0].strip()
+		if planetime.find(" ")!=-1: # assume hr bit on front
+			hrs = planetime.split(" ")[0].strip()
+			assert hrs[-2:] == "hr",hrs
+			planemins = int(hrs[:-2])*60
+			planetime = planetime.split(" ")[1]
+		else:
+			lanemins = 0
+		assert planetime[-3:] == "min",planetime
+		planemins += int(planetime[:-3])
+
+		#print "planemins",planemins
+
+		#print
+		self.response.out.write("Going from %s -> %s<br/>\n"%(start["Address"], end["Address"]))
+		self.response.out.write("<table border=1><th><td>Distance</td><td>CO<sub>2</sub></td><td>Bottles of beer equivalent</td><td>Price</td><td>Time</td></th>\n")
+		self.response.out.write("<tr><td>Train</td><td>%.1f km</td><td>%.1f kg</td><td>%d bottles</td><td></td><td></td></tr>\n"%(path["distance"], traincost,co2InItems(traincost, "beers")))
+		self.response.out.write("<tr><td>Plane</td><td>%.1f km</td><td>%.1f kg</td><td>%d bottles</td><td>&pound;%.2f</td><td>%d minutes</td></tr>\n" %(directDistance, flightcost,co2InItems(flightcost, "beers"), planeprice, planemins))
+		self.response.out.write("</table>\n")
+		self.response.out.write("Plane is %.1f%% worse than train<br/>\n"%(((flightcost-traincost)/traincost)*100.0))
+		self.response.out.write("</body></html>")
+
+
+def main():
+  application = webapp.WSGIApplication([('/', MainHandler)],
+									   debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
+
+
+if __name__ == '__main__':
+  main()
+
+
